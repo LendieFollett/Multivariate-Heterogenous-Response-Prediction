8eac7e0df941eba6debfb1c290be6a8a92c619aa
LendieFollett <lendie.follett@drake.edu>
2022-01-13 15:32:47 -0600

    Update sharedforest_trial.R

--- sharedforest_trial.R
+++ sharedforest_trial.R
@@ -12,10 +12,10 @@ p <- 10
 X <- rnorm(n_train*p) %>% matrix(ncol = p)
 X_norm <- W_norm <- X %>%apply(2, function(x){(x-min(x))/(max(x)-min(x))})
 
-Y <- rnorm(n_train)
+Y <- rnorm(n_train, mean = X[,1]*1)
 Y_scale <- Y
 
-delta <- rbinom(n = n_train, size = 1, prob = 0.5)
+delta <- rbinom(n = n_train, size = 1, prob = (1 + exp(-1.5*X[,2]))^(-1))
 
 
 hypers <- Hypers(X = X, Y = Y_scale, W = W, delta = delta)
@@ -26,5 +26,6 @@ fit_shared <- SharedBart(X = X_norm, Y = Y_scale, W = W_norm, delta = delta,
                          X_test = X_norm, W_test = W_norm, hypers_ = hypers,
                          opts_ = opts)
 
-
-
+#str(fit_shared)
+#dirichlet probability posterior means - should pick out most important
+fit_shared$s %>%apply(2, mean)
