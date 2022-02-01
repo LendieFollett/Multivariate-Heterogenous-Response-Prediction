7d3f6d7bb03aeee4182c48c46950a7987486feb3
LendieFollett <lendie.follett@drake.edu>
2022-02-01 14:41:52 -0600

    Create test_package.R

---
+++ test_package.R
@@ -0,0 +1,4 @@
+library(devtools)
+install_github("LendieFollett/Multivariate-Heterogenous-Response-Prediction/SharedForestBinary-master/SharedForestBinary")
+library(SharedForestBinary)
+library(dplyr)
