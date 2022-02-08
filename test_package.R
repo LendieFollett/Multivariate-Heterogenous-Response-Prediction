ae73a7acc4452c16531eee525f608a647ca54c30
LendieFollett <lendie.follett@drake.edu>
2022-02-08 15:10:51 -0600

    update simulation study

--- test_package.R
+++ test_package.R
@@ -14,16 +14,22 @@ library(ggplot2)
 library(MASS)
 library(BART)
 library(caret)
+library(randomForest)
 
-P = 10
+#"s the variable selection task becomes more difficult, the model which does not share information is far more sensitive to irrelevant predictors than the model which does share"
+P = 50
 n_train = 500
-n_test = 500
-rho <- .3
+n_test = 250
+rho <- 0 #Note: shared bart assumes Z1, Z2 are independent GIVEN the Xs
 nrep <- 10
 d <- array(NA, dim = c(n_train, 2))
 d_test <- array(NA, dim = c(n_test, 2))
 Sigma <-rho*(1-diag(2)) + diag(2)
 results <- list()
+
+#see Section 4. Simulation Study in Linero paper
+sigma_theta <- 1
+
 
 f_fun <- function(W){10*sin(pi*W[,1]*W[,2]) + 20*(W[,3]- 0.5)^2 + 10*W[,4] + 5*W[,5]}
 g0 <- function(x){as.numeric(x > 0)}
@@ -37,8 +43,8 @@ print(paste0("************* Repetition = ", r, " *************"))
 W <- matrix(runif(P*n_train), ncol = P)
 W_test <- matrix(runif(P*n_test), ncol = P)
 
-means <- cbind(f_fun(W), f_fun(-W)) %>% apply(2, m_mean)
-means_test <- cbind(f_fun(W_test), f_fun(-W_test))%>% apply(2, m_mean)
+means <- sigma_theta*(cbind(f_fun(W), f_fun(-W))/20-0.7) #%>% apply(2, m_mean)
+means_test <- sigma_theta*(cbind(f_fun(W_test), f_fun(-W_test))/20-0.7)#%>% apply(2, m_mean)
 
 
 for (i in 1:n_train){d[i,] <- mvrnorm(n = 1, mu=means[i,], Sigma = Sigma)}
@@ -51,6 +57,8 @@ for (i in 1:n_test){d_test[i,] <- mvrnorm(n = 1, mu=means_test[i,], Sigma = Sigm
 delta1_test <- (d_test[,1] > 0) %>%as.numeric()#rbinom(n = n_train, size = 1, prob = (1 + exp(-1.5*W[,1]))^(-1))
 delta2_test <- (d_test[,2] > 0) %>%as.numeric()
 
+
+
 hypers <- Hypers(W,delta1, delta2)
 opts <- Opts(num_burn = 5000, num_thin = 1, num_save = 5000, num_print = 100)
 
@@ -62,14 +70,11 @@ sb <- SharedBartBinary(W = W,
            hypers_ = hypers,
            opts_ = opts)
 
-sb$theta_hat_test1 %>%str()
-#inverse link
-sb$theta_hat_test1 %>% apply(2, mean) %>% pnorm()
 
 #dirichlet probability posterior means - should pick out most important
 s_hat <- sb$s %>%apply(2, mean)
 ggplot() + geom_bar(aes(x = 1:ncol(W), y = s_hat), stat = "identity") +
-  labs(x = "Variable", y = "Inclusion Probability (s-hat)")
+  labs(x = "Variable", y = "Inclusion Probability (s-hat)") + scale_x_continuous(breaks = c(1:ncol(W)))
 
 
 
@@ -93,21 +98,38 @@ rf1 <- randomForest(x = W,
                     xtest = W_test)
 
 rf2 <- randomForest(x = cbind(W, delta1),
-                    y = delta2 %>%as.factor(),
-                    xtest = cbind(W_test,ifelse(rf1$test$predicted==TRUE, 1, 0)))
+                    y = delta2 %>%as.factor())
 
 
-fitmat<- data.frame(delta1_test, delta2_test,
+#True P(d2 = 1) for test set
+P1 <- 1-pnorm(0, mean = means_test[,2], sd= sqrt(Sigma[2,2])) #P(d2 = 1)
+#Random forest estimated P(d2 = 1)
+rf2_P1 <- predict(object = rf2, newdata = cbind(W_test,ifelse(rf1$test$predicted==TRUE, 1, 0)), type = "prob")[,"TRUE"]
+#Shared bart estimated P(d2 = 1)
+sb_P1 = 1-pnorm(0, mean=sb$theta_hat_test2 %>% apply(2, mean), sd = 1 ) #1-P(Z2 < 0| theta-hat, 1)
+#bart estimated P(d2 = 1)
+b_P1 = 1-pnorm(0, mean =b2$yhat.test%>%apply(2, mean), sd = 1)#1-P(Z < 0| theta-hat, 1)
+
+
+fitmat<- data.frame(delta1_test = as.factor(delta1_test),
+                    delta2_test=as.factor(delta2_test),
+           true_P1 = P1, #true probability d_2 = 1
+           sb_P1 = sb_P1, #predicted probability d_2 = 1
+           b_P1 = b_P1,
+           rf_P1 = rf2_P1,
            sb_pred1 = sb$theta_hat_test1%>%apply(2, mean) %>% g0,
            sb_pred2 = sb$theta_hat_test2%>%apply(2, mean) %>% g0,
            b_pred1 = b1$yhat.test %>%apply(2, mean) %>% g0,
            b_pred2 = b2$yhat.test%>%apply(2, mean) %>% g0,
-           rf_pred2 = ifelse(rf2$test$predicted==TRUE, 1, 0))
-fitmat <-  fitmat %>% mutate_all(as.factor)
+           rf_pred2 = (rf2_P1 > 0.5) %>%as.numeric)
+fitmat <-  fitmat %>% mutate_at(vars(matches("pred")),as.factor)
+
+#qplot(p1, sb_P1, data = fitmat)
+
 
 #response 1
-confusionMatrix(fitmat$sb_pred1, fitmat$delta1_test)$byClass[c(1,2)]
-confusionMatrix(fitmat$b_pred1, fitmat$delta1_test)$byClass[c(1,2)]
+#confusionMatrix(fitmat$sb_pred1, fitmat$delta1_test)$byClass[c(1,2)]
+#confusionMatrix(fitmat$b_pred1, fitmat$delta1_test)$byClass[c(1,2)]
 
 #response 2
 results[[r]] <- data.frame(model = c("Shared BART", "Sequential BARTs", "Sequential RF"),
